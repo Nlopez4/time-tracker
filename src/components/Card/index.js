dab7292b11d63c19378f08088ee61dbec73d70c8
nataliel2514 <natalielopez2514@gmail.com>
2021-12-29 20:31:53 -0600

    main card for desktop view styless and info added

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import "./styles.css";
 
+
 function Card() {
     return (
-        <div>
-            <p className="report">Report for</p>
-            <h1 className="name">Natalie Lopez</h1>
-            <ul className="days">
-                <li>Daily</li>
-                <li>Weekly</li>
-                <li>Monthly</li>
-            </ul>
+        <div className="main-card">
+            <div className="card-container">
+                <div class="card-info">
+                    <div class="description">
+                        <img className="photo" src={process.env.PUBLIC_URL + '/images/portfolio.jpg'} />
+                        <p className="report">Report for</p>
+                        <h1 className="name">Natalie Lopez</h1>
+                    </div>
+                </div>
+                <ul className="days">
+                    <li>Daily</li>
+                    <li>Weekly</li>
+                    <li>Monthly</li>
+                </ul>
+            </div>
         </div>
     )
 }
