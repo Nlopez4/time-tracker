87cec44d390216933e122750d3a325e7dbb1df65
nataliel2514 <natalielopez2514@gmail.com>
2021-12-30 11:58:44 -0600

    add cards component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
 import Card from "../../components/Card";
+import Cards from "../../components/Cards";
 
 // Home will contain the entire page of main card and cards
 function Home() {
     return (
         <div>
             <Card />
+            <Cards />
         </div>
     )
 }
